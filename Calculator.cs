7fc278dce4b91b7efc0666ac1ec0014ce9e8c9c5
nayi <nayawww@amikom.ac.id>
2025-03-13 11:33:57 +0700

    Menambahkan fungsi Pengurangan

--- Calculator.cs
+++ Calculator.cs
@@ -3,11 +3,15 @@ class Calculator
 	static void Main(string[] args)
 	{
 		
-	}
 	
 	static int Penambahan(int a, int b)
 	{
 		return a + b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 	
 }
